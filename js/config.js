45d8d37db4a46d8cb50f488b9157340598c83fe7
Aditya Parahsar <aparashar0807@gmail.com>
2026-08-18 22:57:01 +0530

    google auth

--- js/config.js
+++ js/config.js
@@ -1,13 +1,17 @@
-﻿// ═══ FIREBASE CONFIGURATION ═══════════════════════════════════════════════════
-firebase.initializeApp({
+// ═══ FIREBASE CONFIGURATION ═══════════════════════════════════════════════════
+
+const firebaseConfig = {
   apiKey: "AIzaSyBWN0kJn_pkakvVGULCPi9JOzcmVIkoo6Y",
   authDomain: "safe-yatra-157c7.firebaseapp.com",
   projectId: "safe-yatra-157c7",
   storageBucket: "safe-yatra-157c7.firebasestorage.app",
   messagingSenderId: "838865526723",
-  appId: "1:838865526723:web:d66cfeaa1eb3411d0ae776",
-  measurementId: "G-5DGJ7PY8H1"
-});
+  appId: "1:838865526723:web:fb1ba058a39573e00ae776",
+  measurementId: "G-6Q94RMDD76"
+};
+
+// Initialize Firebase using the Compat SDK
+firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
